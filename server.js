ef69309d2cc323cdf4a8b8219d6093b6c2f6dc12
Hellen <hellensoloviy@gmail.com>
2021-02-21 19:50:29 +0200

    add local "log" module;

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+
+//Modules
 var http = require('http'); // Import Node.js core module
+var myLogModule = require('./log_module/log.js'); //import my local module
+
 
 var server = http.createServer(function (req, res) {   //create web server
-    console.log(req);
+    console.log(req); //console log
+    myLogModule.info('Node.js receive a request'); //func of my test module
 
-    if (req.url == '/') { //check the URL of the current request
+
+    if (req.url === '/') { //check the URL of the current request
 
         // set response header
         res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -13,20 +19,20 @@ var server = http.createServer(function (req, res) {   //create web server
         res.end();
 
     }
-    else if (req.url == "/allAvocados") {
+    else if (req.url === "/allAvocados") {
 
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write('<html><html lang={lang} /><body><p>This is an avocados page.</p></body></html>');
         res.end();
 
     }
-    else if (req.url == "/admin") {
+    else if (req.url === "/admin") {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write('<html><html lang={lang} /><body><p>This is admin Page.</p></body></html>');
         res.end();
 
         //add JSON response
-    } else if (req.url == '/avocadoInfo') { //check the URL of the current request
+    } else if (req.url === '/avocadoInfo') { //check the URL of the current request
         res.writeHead(200, { 'Content-Type': 'application/json' });
 
         res.write(JSON.stringify({ message: "Avocado Toast" }));
