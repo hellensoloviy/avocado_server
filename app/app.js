5d76f9a44da7df23e079439835052394a7a34eb0
Hellen <hellensoloviy@gmail.com>
2021-02-21 21:06:09 +0200

    update for the express server from http;
    add simple mysql db integration with presetted data; 
    add Pug for templates;

--- app/app.js
+++ app/app.js
@@ -11,29 +11,3 @@ fs.readFile('test.txt', 'utf8', function (err, data) {
     console.log(data);
 });
 
-
-
-//// ------- Express Server ------------
-// define routes here..
-var app = express();
-
-app.get('/', function (req, res) {
-    res.send('<html><body><h1>Hello World, its me, your favourite Avocado!</h1></body></html>');
-});
-
-app.post('/submit-data', function (req, res) {
-    res.send('POST Request');
-});
-
-app.put('/update-data', function (req, res) {
-    res.send('PUT Request');
-});
-
-app.delete('/delete-data', function (req, res) {
-    res.send('DELETE Request');
-});
-
-//listen to requests
-var server = app.listen(5000, function () {
-    console.log('Node server is running..');
-});
