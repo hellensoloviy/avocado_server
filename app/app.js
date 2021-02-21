cf4bf2208791d0f966ccb08b21764fdadfd00d3d
Hellen <hellensoloviy@gmail.com>
2021-02-21 20:24:36 +0200

    add express server;

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
 
+var express = require('express');
+
 fs.readFile('test.txt', 'utf8', function (err, data) {
 
     debugger;
@@ -9,5 +11,29 @@ fs.readFile('test.txt', 'utf8', function (err, data) {
     console.log(data);
 });
 
-var msg = require('log_module/log.js');
-msg.log('Hello World');
+
+
+//// ------- Express Server ------------
+// define routes here..
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('<html><body><h1>Hello World, its me, your favourite Avocado!</h1></body></html>');
+});
+
+app.post('/submit-data', function (req, res) {
+    res.send('POST Request');
+});
+
+app.put('/update-data', function (req, res) {
+    res.send('PUT Request');
+});
+
+app.delete('/delete-data', function (req, res) {
+    res.send('DELETE Request');
+});
+
+//listen to requests
+var server = app.listen(5000, function () {
+    console.log('Node server is running..');
+});
